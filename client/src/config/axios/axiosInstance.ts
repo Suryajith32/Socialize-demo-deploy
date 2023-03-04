c41805d7db75154aa58edce98546bded1713bd20
Suryajith32 <suryajithms32@gmail.com>
2023-03-04 16:52:04 +0530

    updated api route

--- client/src/config/axios/axiosInstance.ts
+++ client/src/config/axios/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"http://localhost:4000"
+    baseURL:"https://socialize-server-89m4.onrender.com"
 })
 axios.interceptors.request.use(
     function(config){
